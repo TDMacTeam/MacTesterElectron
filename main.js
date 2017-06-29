14d616b36b37a2e8330fc100cbe6428bc6e595e0
TDMacTeam <n.olander@techdefenders.com>
2017-06-29 10:46:29 -0400

    Can get information about system

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
+const macInfo = require('./macInteraction.js')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -19,6 +20,7 @@ function createWindow () {
 
   // Open the DevTools.
   // win.webContents.openDevTools()
+  macInfo.cpuInfo()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
